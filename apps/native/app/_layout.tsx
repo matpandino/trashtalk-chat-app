ac73f89c2dbb8cdb6739440170af83e312b6abc0
Mateus Pandino <matpandino@gmail.com>
2024-10-19 22:33:28 -0300

    add react native paper

--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -2,15 +2,25 @@ import { Slot } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { UserStoreProvider } from "../utils/providers/user-store-provider";
 import { ChattingStoreProvider } from "../utils/providers/chatting-store-provider";
+import { DefaultTheme, PaperProvider } from "react-native-paper";
 
 const queryClient = new QueryClient();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+  },
+};
 
 export default function Layout() {
   return (
     <UserStoreProvider>
       <ChattingStoreProvider>
         <QueryClientProvider client={queryClient}>
-          <Slot />
+          <PaperProvider theme={theme}>
+            <Slot />
+          </PaperProvider>
         </QueryClientProvider>
       </ChattingStoreProvider>
     </UserStoreProvider>
--- apps/native/babel.config.js
+++ apps/native/babel.config.js
@@ -1,7 +1,12 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: ['expo-router/babel'],
+    presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
+    plugins: ["expo-router/babel"],
+    env: {
+      production: {
+        plugins: ["react-native-paper/babel"],
+      },
+    },
   };
 };
