fe320cd1f40d61600d2260139e33c630a5ee2eea
Mateus Pandino <matpandino@gmail.com>
2024-10-20 18:37:36 -0300

    add stack screen options

--- apps/native/app/chatting/_layout.tsx
+++ apps/native/app/chatting/_layout.tsx
@@ -1,5 +1,16 @@
 import { Stack } from "expo-router";
+import { Text } from "react-native-paper";
+import { useAppTheme } from "../../utils/theme";
 
 export default function Layout() {
-  return <Stack />;
+  const appTheme = useAppTheme();
+  return (
+    <Stack
+      screenOptions={{
+        headerBackTitle: "Back",
+        headerStyle: {backgroundColor: appTheme.colors.surfaceVariant},
+        headerTitle: ({children}) => <Text variant="titleMedium">{children}</Text>,
+      }}
+    />
+  );
 }
