247d6e8f8d4b58c4f260f56935c4811a17e49d06
Mateus Pandino <matpandino@gmail.com>
2024-10-21 09:51:28 -0300

    add new colors

--- apps/native/utils/theme.ts
+++ apps/native/utils/theme.ts
@@ -1,5 +1,6 @@
 import { darken } from "polished";
 import { MD3DarkTheme, useTheme } from "react-native-paper";
+import { white } from "react-native-paper/lib/typescript/styles/themes/v2/colors";
 import { MD3Colors } from "react-native-paper/lib/typescript/types";
 
 export const useAppTheme = () => useTheme<AppTheme>();
@@ -13,7 +14,9 @@ const defaultColors: MD3Colors = {
 };
 
 const customColors = {
+    white: "#ffff",
     red: "#f25757",
+    strongGreen: '#3f7a35',
     green: "#85f781",
     secondaryBg: darken(0.3, defaultColors.secondary),
     primaryBg: darken(0.3, defaultColors.primary),
