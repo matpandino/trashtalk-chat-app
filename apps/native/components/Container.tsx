455159aa48145dbb7fc8b51227f62b4db72cc898
Mateus Pandino <matpandino@gmail.com>
2024-10-20 22:22:30 -0300

    add keyboard aware view

--- apps/native/components/Container.tsx
+++ apps/native/components/Container.tsx
@@ -1,21 +1,31 @@
 import React from "react";
-import { SafeAreaView, View, StyleSheet } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  StyleSheet,
+  KeyboardAvoidingView,
+} from "react-native";
 import { useAppTheme } from "../utils/theme";
+import { useHeaderHeight } from "@react-navigation/elements";
 
 interface ContainerProps extends React.ComponentProps<typeof View> {}
 
 export const Container: React.FC<ContainerProps> = (props) => {
   const appTheme = useAppTheme();
-  
+  const height = useHeaderHeight()
   return (
-    <SafeAreaView style={[styles.safeArea, { backgroundColor: appTheme.colors.onSurfaceDisabled }]}>
-      <View
-        style={styles.container}
-        {...props}
+    <KeyboardAvoidingView style={{ flex: 1 }} keyboardVerticalOffset={height} behavior="padding">
+      <SafeAreaView
+        style={[
+          styles.safeArea,
+          { backgroundColor: appTheme.colors.background },
+        ]}
       >
-        {props.children}
-      </View>
-    </SafeAreaView>
+        <View style={styles.container} {...props}>
+          {props.children}
+        </View>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 };
 
